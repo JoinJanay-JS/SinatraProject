48e086644f4bfc3504ab4f389ed740f91a6ff110
Janay Bailey <44117424+JoinJanay-JS@users.noreply.github.com>
2020-01-20 01:05:18 -0500

    Enter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,22 @@ class UsersController < ApplicationController
     get '/signup' do
       redirect to '/users' if is_logged_in?
       #go to users page and allow users to select student
-      erb :"users/signup"
+      erb :'users/signup'
     end
   
     post '/signup' do
       user = Users.new(username: params["username"], email: params["email"], password: params["password"])
-      if users.username.empty? || users.password.empty?
+      if user.username.empty? || user.password.empty?
         @error = "Not so fast, Sneaky Teacher!"
-        erb :'user/signup'
+        erb :'users/signup'
       elsif  
         Users.find_by(username: user.username) 
         @error = "Whoopsie! It looks like you're already a Sneaky Teacher. Please enter a new email or log in to continue." 
       else
-        new_user= User.create(user)
+        new_user= Users.create(username: params["username"], email: params["email"], password: params["password"])
         session[:user_id] = new_user.id   
-        users.save 
-        redirect '/users'
+        user.save 
+        redirect '/users/show'
       end 
       end 
 
