a393a1a3ca164837eda5c68b4f4e46fb368af1c1
Janay Bailey <44117424+JoinJanay-JS@users.noreply.github.com>
2020-01-22 23:19:40 -0500

    Enter

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,10 +1,17 @@
 class ActivitiesController < ApplicationController
 
     get '/activities' do 
-        activities = Activities.new(title: params["title"], supplies: params["supplies"], instructions: params["instructions"])
+        @activities = Activities.all.reverse
+        erb '/activities/index'
     end 
 
     post '/activities' do 
+        activities = Activities.new(title: params["title"], supplies: params["supplies"], instructions: params["instructions"])
     end 
+  
+      get '/activities/:id' do
+        @activities = Activities.find(params["id"])
+        erb :"activities/show"
+      end
 
 end 
