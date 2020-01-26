a12eeecd750a82516f4f68cb74269ad3618561db
Janay Bailey <44117424+JoinJanay-JS@users.noreply.github.com>
2020-01-25 20:36:22 -0500

    Enter

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,5 +1,7 @@
 class Users < ActiveRecord::Base
+    has_one :username
     has_many :students
-    has_many :titles, through: :students
+    has_many :activities, through: :students
+    has_many :titles, through: :activities
     has_secure_password
   end
