fac8efc49ed500c98106dd2521bcec5ceceb6314
Janay Bailey <44117424+JoinJanay-JS@users.noreply.github.com>
2020-04-20 07:41:32 -0400

    Enter

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,44 +8,81 @@ class StudentsController < ApplicationController
     post '/students' do
       students = Student.new(params["student"])
       students.save
-      erb :"students/index"
+      redirect to "/users/#{user.id}"
     end
 
     get '/students/new' do
-      erb :"students/new"
+        if !is_logged_in?
+          redirect '/'
+        end
+      erb :'students/new'
     end 
 
     get '/student/:id' do 
       #binding.pry
-      @student = Student.find_by(id: params[:id])
+      if !is_logged_in?
+        redirect '/'  
+      end
+        @student = Student.find_by(id: params[:id])
+        if !@student
+          redirect '/'
+        end
      erb :'students/show'
     end 
 
-    get '/student/:id/edit' do
+    #get '/student/:id/edit' do
      # binding.pry
-      @student = student.find_by_id(params[:id])
+   #student = Student.find_by_id(params[:id])
     # if current_user.id != @student.id
-      erb :'/students/show'
-  end 
+    #  erb :'/students/edit'
+ # end 
     
-      patch '/student/:id' do 
-        @student = student.find_by_id(params[:id])
-        @student.name = params[:name]
-        @student.age = params[:age]
-        @student.save
-      flash[:message] = "Successfully updated your Student!"
-        redirect to "/student/#{student.id}"
-    end
+     # patch '/student/:id' do 
+      #  @student = student.find_by_id(params[:id])
+      #  @student.name = params[:name]
+      #  @student.age = params[:age]
+      #  @student.save
+     # flash[:message] = "Successfully updated your Student!"
+     #   redirect to "/student/#{student.id}"
+   # end
   
-  post '/student/:id/edit' do
-    @student.update(name: params["name"], age: params["age"])
-    flash[:message] = "Successfully updated your Student!"
-      erb :'/students/show'
-  end 
+ # post '/student/:id/edit' do
+ #   @student.update(name: params["name"], age: params["age"])
+ #   flash[:message] = "Successfully updated your Student!"
+   #   erb :'/students/show'
+ # end 
 
-    delete '/student/:id/delete' do
-      student.destroy
-      flash[:message] = "Your student has been deleted"
-      redirect to '/students'
-    end
+  #  delete '/student/:id/delete' do
+   #   student.destroy
+    #  flash[:message] = "Your student has been deleted"
+    #  redirect to '/students'
+   # end
+ # end
+
+
+  get '/students/:id/edit' do
+    @student = student.find_by(id: params[:id])
+  if !is_logged_in? || !@student || @student.user != current_user
+    redirect '/'
   end
+  erb :'/students/edit'
+end
+
+patch '/students/:id' do
+  student = student.find_by(id: params[:id])
+  if student && student.user == current_user
+    student.update(params[:student])
+    redirect to "/students/#{student.id}"
+  else
+    redirect to "/students"
+  end
+end
+
+delete '/students/:id/delete' do
+  student = student.find_by(id: params[:id])
+  if student && student.user == current_user
+    student.destroy
+  end
+  redirect to '/students'
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,5 @@
 class UsersController < ApplicationController
 
-  get '/signup' do
-    erb :'users/signup'
-  end
-
-  post '/users' do
-      user = User.create(username: params["username"], email: params["email"], password: params["password"]  )
-      if user.valid?
-      session[:user_id] = user.id 
-      redirect to "/users/#{user.id}"
-    # if new_user = User.find_by(username: params["username"]) 
-    #  @error = "Whoopsie! It looks like you're already a Sneaky Teacher. Please enter a new email or log in to continue." 
-    #  erb :'users/login'
-   end 
-  end 
-
   get '/users' do
     if is_logged_in?
       @users = User.all
@@ -24,7 +9,19 @@ class UsersController < ApplicationController
     erb :'users/index'
   end
 
-  get '/users/:id' do 
+  get '/signup' do
+    erb :'users/signup'
+  end
+
+  post '/users' do
+      user = User.create(username: params["username"], email: params["email"], password: params["password"]  )
+      if user.valid?
+      session[:user_id] = user.id 
+      redirect to "/users/#{user.id}"
+   end 
+  end 
+
+  get '/users/:id/students' do 
     user = User.create(username: params["username"], email: params["email"], password: params["password"]  )
     if is_logged_in? && User.find_by(id: params[:id])
     @user = User.find_by(id: params[:id])
@@ -33,11 +30,6 @@ class UsersController < ApplicationController
       redirect to '/' 
     end
    erb :'/users/show'
-  end
-
-    get '/users/:id/students' do 
-        @user = User.find_by(id: params["id"])
-        @students = @user.students
-        erb :'/users/index'
-    end 
+    end
+    
   end
